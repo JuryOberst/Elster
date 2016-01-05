cbeae3e6102931aa3863561bb8a47239808a5063
Mark Junker <m.junker@fubar-dev.de>
2016-01-05 16:45:51 +0100

    Fehlende Dateien hinzugefügt

---
+++ Dataline.Elster.Anmeldung/UStA201101/usta_AnmeldungssteuernCTypeExtension.cs
@@ -0,0 +1,12 @@
+﻿using Dataline.Elster.Basis;
+
+namespace Dataline.Elster.Anmeldung.UStA201101
+{
+    public partial class usta_AnmeldungssteuernCType : IAnmeldungssteuern
+    {
+        string IAnmeldungssteuern.DatensatzVersion
+        {
+            get { return XmlSchemaUtilities.GetEnumXmlValue(version); }
+        }
+    }
+}
